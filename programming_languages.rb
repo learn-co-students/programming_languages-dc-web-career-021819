a5c9b15239998fe0a59667d72d7d5ce0776c9674
Mickey <micksheridan.24@gmail.com>
2019-01-30 09:50:30 -0500

    submit

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,35 @@
+require "pry"
+
 def reformat_languages(languages)
   # your code here
+  # style, lang, type
+  # lang type type
+
+  new_hash = {}
+  
+
+  languages[:oo].each do |k, v|
+  
+    new_hash[k] = v  #{:ruby => {type: "interpet"}}
+    new_hash[k][:style] = [:oo]
+    new_hash
+  end
+  languages[:functional].each do |k, v|
+  
+    new_hash[k] = v  #{:ruby => {type: "interpet"}}
+   
+    new_hash[k][:style] =[:functional]
+    if languages[:oo].include? (k)
+      new_hash[k][:style] << :oo
+    end
+
+    
+    new_hash
+  end
+
+
+  new_hash
+
+
+  
 end
