054dfe5f028bbb584c7f396ea897634a6e7024d4
Ammar Ali <funnyguy118@gmail.com>
2019-02-04 16:59:36 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,41 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  
+  languages.each do |styles, value_hash|
+    value_hash.each do |language_name, attributes_hash|
+      attributes_hash.each do |type, type_value|
+        
+        
+        if new_hash[language_name].nil?
+          #if language_name :ruby doesn't exist in new hash
+          new_hash[language_name] = {}  #initiate a new hash for the value of :ruby
+        end
+        
+        #new_hash[language_name] = {type => type_value} #, :style => []}
+      
+
+
+        
+        new_hash[language_name][:style] ||= []
+        new_hash[language_name][:style] << styles
+
+        
+        if new_hash[language_name][type].nil?
+          #if :ruby language key doesn't have a :type key nested within it
+          new_hash[language_name][type] = type_value  #set this :type key to equal the str "interpreted"
+        end
+        
+      end
+      
+      
+    end
+    
+  end
+  
+  return new_hash
 end
+
+
+
